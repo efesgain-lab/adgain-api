6f26d43d8f790367c27129cd259dddb69e566c48
efesgain-lab <efesgain@gmail.com>
2026-04-21 19:49:50 -0400

    Update server.jsfix: restaurar ponto-e-virgula em const app = express()

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 
-const app = express() 
+const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
