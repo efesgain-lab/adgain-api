dc71c183eab9832e6ba03561c892a7360510ad17
efesgain-lab <efesgain@gmail.com>
2026-04-27 11:34:53 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config() // init
    
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
+const { Pool } = require('pg'); // v2
 
 const app = express();
 const port = process.env.PORT || 3000;
